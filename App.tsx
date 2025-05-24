91d7fade0c0de976ab67d58f9d301bc5fc362f4e
Roni Pal <roniofficial41@gmail.com>
2025-05-24 09:58:11 +0530

    Update App.tsx: header and navigation config changes

--- App.tsx
+++ App.tsx
@@ -1,131 +1,120 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
+import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import Home from './src_pages/Home';
+import Icon from 'react-native-vector-icons/FontAwesome5.js';
+import Connect from './src_pages/Connect';
+import Control from './src_pages/Control';
+import Products from './src_pages/Products';
+import Updates from './src_pages/Updates';
+import Stats from './src_pages/Stats';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+//Initialize navigation
+const Tab = createBottomTabNavigator();
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the recommendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
-  const safePadding = '5%';
+//main app
+const App = () => {
+  //Variables declearation
+  const btnActiveColor = 'black';
+  const btnColor = '#8f8f96';
 
   return (
-    <View style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header/>
-        </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
+    <NavigationContainer>
+      <SafeAreaView style={styles.container}>
+        <Tab.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            tabBarActiveTintColor: btnActiveColor,
+            tabBarInactiveTintColor: btnColor,
+            tabBarStyle: {
+              height: 50,
+              alignItems: 'center',
+              justifyContent: 'center',
+            },
+            tabBarLabelStyle: {
+              fontSize: 10,
+              fontWeight: 900,
+            },
+            headerTitle: () => <Text style={styles.headerText}>RX Mat Pro</Text>,
+            headerTitleAlign: "left",
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </View>
+          {/* Home Nav */}
+          <Tab.Screen
+            name="Home"
+            component={Home}
+            options={{
+              tabBarIcon: ({color}) => (
+                <Icon name="home" size={15} color={color} />
+              ),
+            }}
+          />
+          {/* Connect Nav */}
+          <Tab.Screen
+            name="Connect"
+            component={Connect}
+            options={{
+              tabBarIcon: ({color}) => (
+                <Icon name="rss" size={15} color={color} />
+              ),
+            }}
+          />
+          {/* Control Nav */}
+          <Tab.Screen
+            name="Control"
+            component={Control}
+            options={{
+              tabBarIcon: ({color}) => (
+                <Icon name="sliders-h" size={15} color={color} />
+              ),
+            }}
+          />
+          {/* Products Nav */}
+          <Tab.Screen
+            name="Products"
+            component={Products}
+            options={{
+              tabBarIcon: ({color}) => (
+                <Icon name="shopping-bag" size={15} color={color} />
+              ),
+            }}
+          />
+          {/* Updates Nav */}
+          <Tab.Screen
+            name="Updates"
+            component={Updates}
+            options={{
+              tabBarIcon: ({color}) => (
+                <Icon name="download" size={15} color={color} />
+              ),
+            }}
+          />
+          {/* Stats Nav */}
+          <Tab.Screen
+            name="Stats"
+            component={Stats}
+            options={{
+              tabBarIcon: ({color}) => (
+                <Icon name="chart-line" size={15} color={color} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </SafeAreaView>
+    </NavigationContainer>
   );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+};
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  headerText:{
+    color: "#0f766e",
+    fontWeight: 900,
+    fontSize:25,
+  }
+});
